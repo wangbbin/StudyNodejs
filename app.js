2ec10b934d8c22a2a6ef69be27f782bff52910d5
wangbbin <wbb499477096@qq.com>
2016-08-27 22:10:19 +0800

    n1.1: http post

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var ROOT_DIR = 'html/';
+
+var message = ['a', 'b', 'ccc'];
 http.createServer(function(req, res){
 
+	/*//static
 	var urlObj = url.parse(req.url, true, false);
 	console.dir(urlObj);
 	fs.readFile(ROOT_DIR + urlObj.pathname + 'index.html', function(err, data){
@@ -14,6 +17,32 @@ http.createServer(function(req, res){
 		}
 		res.writeHead(200);
 		res.end(data);
+	});*/
+
+	/*res.setHeader('Content-Type','text/html');
+	res.writeHead(200);
+	res.write('<html><head><title>Simple Http S</title></head>');
+	res.write('<body>');
+	for (var i = message.length - 1; i >= 0; i--) {
+		res.write('\n<h1>' + message[i] + '</h1>');
+	};
+
+	res.end('</body></html>');*/
+
+	var jsonData = '';
+	req.on('data', function(chunk){
+		jsonData += chunk;
+	});
+
+	req.on('end', function(){
+		console.log('req end' + jsonData);
+		var reqObj = JSON.parse(jsonData);
+		var resObj = {
+			message: 'Hello' + reqObj.name,
+			question: 'Are you a good ' + reqObj.occupation + '?'
+		};
+		res.writeHead(200);
+		res.end(JSON.stringify(resObj));
 	});
  
 }).listen(8080);
--- client.js
+++ client.js
@@ -1,9 +1,22 @@
 var http = require('http');
 var options = {
 	hostname: 'localhost',
-	port: 8080/*,
-	path: '/'*/
+	port: 8080,
+	path: '/',
+	method: 'post'
 };
+
+/*function handleReponse(response){
+	var serverData = '';
+	response.on('data', function(chunk){
+		serverData += chunk;
+	});
+	response.on('end', function(){
+		console.log('response status: ' , response.statusCode);
+		console.log('response Header: ', response.headers)
+		console.log(serverData);
+	});
+}*/
 
 function handleReponse(response){
 	var serverData = '';
@@ -11,10 +24,15 @@ function handleReponse(response){
 		serverData += chunk;
 	});
 	response.on('end', function(){
-		console.log(serverData);
+		var resObj = JSON.parse(serverData);
+		console.log('Raw response: ' , resObj);
+		console.log('Message: ', resObj.message);
+		console.log('Q :' , resObj.question);
 	});
 }
 
-http.request(options, function(response){
+var req = http.request(options, function(response){
 	handleReponse(response);
-}).end();
+});
+req.write(JSON.stringify({name: "wang", occupation: "Angular"}));
+req.end();
