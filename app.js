31b1d5b5b92d58f3ce96dae5c5f241d9c61d0af3
wangbbin <wbb499477096@qq.com>
2016-08-28 16:31:31 +0800

    n1.2: http server

--- app.js
+++ app.js
@@ -1,48 +1,56 @@
-var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var ROOT_DIR = 'html/';
+var queryString = require('querystring');
 
-var message = ['a', 'b', 'ccc'];
-http.createServer(function(req, res){
+function sendResponse(weatherData, res){
+	var page = '<html><head><title>External Example</title></head>' +
+							'<body>' +
+							'<form method="post">' +
+							'City: <input name="city">' +
+							'<input type="submit" value="Get Weather">' +
+							'</form>';
+	if(weatherData){
+		page += '<h1>weather info</h1><p>' +weatherData+'</p>'; 
+	}
+	page += '</body></html>';
+	res.end(page);
+}
 
-	/*//static
-	var urlObj = url.parse(req.url, true, false);
-	console.dir(urlObj);
-	fs.readFile(ROOT_DIR + urlObj.pathname + 'index.html', function(err, data){
-		if(err){
-			res.writeHead(404);
-			res.end(JSON.stringify(err));
-			return;
-		}
-		res.writeHead(200);
-		res.end(data);
-	});*/
+function parseWeather(weatherResponse, res){
+	var weatherData = 'how beaty';
+	weatherResponse.on('data', function(chunk){
+		weatherData += chunk;
+	});
+	weatherResponse.on('end', function(){
+		sendResponse(weatherData, res);
+	});
+}
 
-	/*res.setHeader('Content-Type','text/html');
-	res.writeHead(200);
-	res.write('<html><head><title>Simple Http S</title></head>');
-	res.write('<body>');
-	for (var i = message.length - 1; i >= 0; i--) {
-		res.write('\n<h1>' + message[i] + '</h1>');
+function getWeather(city, res){
+
+	var options = {
+		host: 'api.openweathermap.map',
+		path: '/data/2.5/weather?q=' + city
 	};
+	http.request(options, function(weatherResponse){
+		console.log('getWeather back');
+		parseWeather(weatherResponse, res);
+	}).end();
+}
 
-	res.end('</body></html>');*/
+http.createServer(function(req, res){
+	console.log(req.method);
+	if(req.method == 'POST'){
+		var reqData = '';
+		req.on('data', function(chunk){
+			reqData += chunk;
+		});
+		req.on('end', function(){
+			var postParams = queryString.parse(reqData);
+			getWeather(postParams.city, res);
+		});
 
-	var jsonData = '';
-	req.on('data', function(chunk){
-		jsonData += chunk;
-	});
-
-	req.on('end', function(){
-		console.log('req end' + jsonData);
-		var reqObj = JSON.parse(jsonData);
-		var resObj = {
-			message: 'Hello' + reqObj.name,
-			question: 'Are you a good ' + reqObj.occupation + '?'
-		};
-		res.writeHead(200);
-		res.end(JSON.stringify(resObj));
-	});
- 
+	}else{
+		sendResponse(null, res);
+	}
 }).listen(8080);
