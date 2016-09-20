8ef50a4d376136068a843508312b740c26254b6a
wangbbin <wbb499477096@qq.com>
2016-09-20 23:49:49 +0800

    n4.7 blog
    comments

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto'),
     User = require('../models/user.js'),
-    Post = require('../models/post.js');
+    Post = require('../models/post.js'),
+    Comment = require('../models/comment.js');
 var multer  = require('multer');
 //原版本使用方法
 //app.use(multer({
@@ -250,6 +251,28 @@ module.exports = function(app) {
       }
       req.flash('success', '删除成功!');
       res.redirect('/');
+    });
+  });
+
+  app.post('/u/:name/:day/:title', function (req, res) {
+    var date = new Date(),
+        time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
+               date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    var comment = {
+        name: req.body.name,
+        email: req.body.email,
+        website: req.body.website,
+        time: time,
+        content: req.body.content
+    };
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function (err) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('back');
+      }
+      req.flash('success', '留言成功!');
+      res.redirect('back');
     });
   });
 
