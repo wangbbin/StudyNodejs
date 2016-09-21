4a87b3b4f9386243a066164e65de87fb31162e42
wangbbin <wbb499477096@qq.com>
2016-09-22 00:08:18 +0800

    n4.11 blog
    links & 404 & reprint
    not photo

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -142,7 +142,7 @@ module.exports = function(app) {
   app.post('/post', function (req, res) {
     var currentUser = req.session.user,
     tags = [req.body.tag1, req.body.tag2, req.body.tag3],
-    post = new Post(currentUser.name, req.body.title, tags, req.body.post);
+    post = new Post(currentUser.name, currentUser.head, req.body.title, tags, req.body.post);
     post.save(function (err) {
       if (err) {
         req.flash('error', err); 
@@ -175,6 +175,16 @@ module.exports = function(app) {
     req.flash('success', '文件上传成功!');
     res.redirect('/upload');
   });
+
+  app.get('/links', function (req, res) {
+    res.render('links', {
+      title: '友情链接',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+
   app.get('/search', function (req, res) {
     Post.search(req.query.keyword, function (err, posts) {
       if (err) {
@@ -327,12 +337,40 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/reprint/:name/:day/:title', checkLogin);
+  app.get('/reprint/:name/:day/:title', function (req, res) {
+    Post.edit(req.params.name, req.params.day, req.params.title, function (err, post) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect(back);
+      }
+      var currentUser = req.session.user,
+          reprint_from = {name: post.name, day: post.time.day, title: post.title},
+          reprint_to = {name: currentUser.name, head: currentUser.head};
+      Post.reprint(reprint_from, reprint_to, function (err, post) {
+        if (err) {
+          req.flash('error', err); 
+          return res.redirect('back');
+        }
+        req.flash('success', '转载成功!');
+        console.log('reprint:',post)
+        var url = encodeURI('/u/' + post.name + '/' + post.time.day + '/' + post.title);
+        //跳转到转载后的文章页面
+        res.redirect(url);
+      });
+    });
+  });
+
   app.post('/u/:name/:day/:title', function (req, res) {
     var date = new Date(),
         time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
                date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    var md5 = crypto.createHash('md5'),
+        email_MD5 = md5.update(req.body.email.toLowerCase()).digest('hex'),
+        head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48"; 
     var comment = {
         name: req.body.name,
+        head: head,
         email: req.body.email,
         website: req.body.website,
         time: time,
@@ -347,6 +385,10 @@ module.exports = function(app) {
       req.flash('success', '留言成功!');
       res.redirect('back');
     });
+  });
+
+  app.use(function (req, res) {
+    res.render("404");
   });
 
   function checkLogin(req, res, next) {
