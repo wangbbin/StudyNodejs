9afa36eef707f0e4db6bf2bfad67ea789f7b69c9
wangbbin <wbb499477096@qq.com>
2016-10-10 21:09:14 +0800

    6 websocket
    chat & offline & re-connect

--- socket/N-chat/app.js
+++ socket/N-chat/app.js
@@ -43,34 +43,11 @@ app.post('/signin', function (req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res) {
+  console.log('finish ending');
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.status(404).json(err);
 });
 
 var http = require('http');
@@ -85,6 +62,7 @@ io.sockets.on('connection', function (socket) {
   //有人上线
   socket.on('online', function (data) {
     //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
+    console.log(socket.name)
     socket.name = data.user;
     //users 对象中不存在该用户名则插入该用户名
     if (!users[data.user]) {
@@ -92,6 +70,41 @@ io.sockets.on('connection', function (socket) {
     }
     //向所有用户广播该用户上线信息
     io.sockets.emit('online', {users: users, user: data.user});
+  });
+
+  //有人发话
+  socket.on('say', function (data) {
+    ['a','b','c'].forEach(function(item){
+        console.log(item)
+    });
+    
+    if (data.to == 'all') {
+      //向其他所有用户广播该用户发话信息
+      socket.broadcast.emit('say', data);
+    } else {
+      //向特定用户发送该用户发话信息
+      //clients 为存储所有连接对象的数组
+
+      var clients = io.sockets.clients();
+      //遍历找到该用户
+      clients.forEach(function (client) {
+        if (client.name == data.to) {
+          //触发该用户客户端的 say 事件
+          client.emit('say', data);
+        }
+      });
+    }
+  });
+
+  //有人下线
+  socket.on('disconnect', function() {
+    //若 users 对象中保存了该用户名
+    if (users[socket.name]) {
+      //从 users 对象中删除该用户名
+      delete users[socket.name];
+      //向其他所有用户广播该用户下线信息
+      socket.broadcast.emit('offline', {users: users, user: socket.name});
+    }
   });
 });
 
--- socket/N-chat/public/javascripts/chat.js
+++ socket/N-chat/public/javascripts/chat.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+	
+	$(window).keydown(function (e) {
+	  if (e.keyCode == 116) {
+	    if (!confirm("刷新将会清除所有聊天记录，确定要刷新么？")) {
+	      e.preventDefault();
+	    }
+	  }
+	});
   var socket = io.connect();
   var from = $.cookie('user');//从 cookie 中读取用户名，存于变量 from
 	var to = 'all';//设置默认接收对象为"所有人"
@@ -16,6 +24,44 @@ $(document).ready(function() {
 	  flushUsers(data.users);
 	  //显示正在对谁说话
 	  showSayTo();
+	});
+
+	socket.on('say', function (data) {
+	  //对所有人说
+	  if (data.to == 'all') {
+	    $("#contents").append('<div>' + data.from + '(' + now() + ')对 所有人 说：<br/>' + data.msg + '</div><br />');
+	  }
+	  //对你密语
+	  if (data.to == from) {
+	    $("#contents").append('<div style="color:#00f" >' + data.from + '(' + now() + ')对 你 说：<br/>' + data.msg + '</div><br />');
+	  }
+	});
+
+	socket.on('offline', function (data) {
+	  //显示系统消息
+	  var sys = '<div style="color:#f00">系统(' + now() + '):' + '用户 ' + data.user + ' 下线了！</div>';
+	  $("#contents").append(sys + "<br/>");
+	  //刷新用户在线列表
+	  flushUsers(data.users);
+	  //如果正对某人聊天，该人却下线了
+	  if (data.user == to) {
+	    to = "all";
+	  }
+	  //显示正在对谁说话
+	  showSayTo();
+	});
+	//服务器关闭
+	socket.on('disconnect', function() {
+	  var sys = '<div style="color:#f00">系统:连接服务器失败！</div>';
+	  $("#contents").append(sys + "<br/>");
+	  $("#list").empty();
+	});
+
+	//重新启动服务器
+	socket.on('reconnect', function() {
+	  var sys = '<div style="color:#f00">系统:重新连接服务器！</div>';
+	  $("#contents").append(sys + "<br/>");
+	  socket.emit('online', {user: from});
 	});
 
 	//刷新用户在线列表
@@ -40,7 +86,6 @@ $(document).ready(function() {
 	      showSayTo();
 	    }
 	  });
-
 	}
 
   //显示正在对谁说话
@@ -54,4 +99,23 @@ $(document).ready(function() {
 	  var time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()) + ":" + (date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds());
 	  return time;
 	}
+
+	//发话
+	$("#say").click(function() {
+	  //获取要发送的信息
+	  var $msg = $("#input_content").html();
+	  if ($msg == "") return;
+	  //把发送的信息先添加到自己的浏览器 DOM 中
+	  if (to == "all") {
+	    $("#contents").append('<div>你(' + now() + ')对 所有人 说：<br/>' + $msg + '</div><br />');
+	  } else {
+	    $("#contents").append('<div style="color:#00f" >你(' + now() + ')对 ' + to + ' 说：<br/>' + $msg + '</div><br />');
+	  }
+	  //发送发话信息
+	  socket.emit('say', {from: from, to: to, msg: $msg});
+	  //清空输入框并获得焦点
+	  $("#input_content").html("").focus();
+	});
+
+
 });
