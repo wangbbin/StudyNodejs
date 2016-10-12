4bc6e0a2b5db61f61edabe81cd3501269bf3c374
wangbbin <wbb499477096@qq.com>
2016-10-13 00:08:16 +0800

    crawler 1.5
    get & save article

---
+++ crawler/config.js
@@ -0,0 +1,14 @@
+// MySQL数据库连接配置
+var mysql = require('mysql');
+exports.db = mysql.createConnection({
+  host:            '127.0.0.1',   // 数据库地址
+  port:            3306,          // 数据库端口
+  database:        'sina_article',   // 数据库名称
+  user:            'root',        // 数据库用户
+  password:        'root'             // 数据库用户对应的密码
+});
+
+// 博客配置
+exports.sinaBlog = {
+  url: 'http://blog.sina.com.cn/u/1776757314'  // 博客首页地址
+};
